d1b0947ef9c3cd4e416df64c030549429eb7d037
musabbihaz <musabbihaz@users.noreply.github.com>
2021-06-15 20:56:57 +0000

    deploy: d7cf9f53c6d49f3d257ce0728cce26473ef5b92a

--- branch/edits_doc/_static/documentation_options.js
+++ branch/edits_doc/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.0.dev3984',
+    VERSION: '3.0.dev3985',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
